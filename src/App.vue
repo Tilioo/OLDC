f68afee3aea819ff938dfb19bf49d13693460f5c
Christian Giglio <c.giglio@solingen.de>
2022-05-05 14:05:40 +0200

    cleaned before ol

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MapFrame />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MapFrame from './components/MapFrame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MapFrame,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MapFrame.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gray-600 text-white w-1/2 m-auto mt-12">
+    <div class="text-center border-2 border-black">
+      <h1>Map</h1>
+    </div>
+    <div class="text-center bg-blue-700">
+    <h1>Test</h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MapFrame",
+};
+</script>
