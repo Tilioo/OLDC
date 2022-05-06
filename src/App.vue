52b4ab05502714ef5cba5ecb07b75b29fd63bfff
Christian Giglio <c.giglio@solingen.de>
2022-05-06 12:04:01 +0200

    Changed Layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="grid grid-cols-3 gap-4 p-4 h-screen w-screen">
-    <div class="bg-green-100 row-span-2 col-span-2"><MapFrame /></div>
-    <div class="bg-green-100">View</div>
-    <div class="bg-green-100">Edit</div>
+  <div class="bg-gradient-to-r from-gray-700 via-gray-900 to-black ">
+    <div class="grid grid-cols-3 gap-4 p-4 h-screen w-screen">
+      <div class="bg-gray-400 col-span-3 rounded-lg m-3">Head</div>
+      <div class="row-span-6 col-span-2 m-3"><MapFrame /></div>
+      <div class="row-span-3 bg-gray-400 rounded-lg m-3">View</div>
+      <div class="row-span-3 bg-gray-400 rounded-lg m-3">Edit</div>
+    </div>
   </div>
 </template>
 
